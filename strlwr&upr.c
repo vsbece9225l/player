bbe186bd6e1725184f1a8748ed5164b782bd4296
vsbece9225l <30829561+vsbece9225l@users.noreply.github.com>
2018-03-15 14:26:00 +0530

    Create strlwr&upr.c

---
+++ strlwr&upr.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+     char str[50];
+     printf("\nenter the string");
+     scanf("%d",&str);
+     printf("%s",strlwr(str));
+     printf("%s",strupr(str));
+     return 0;
+}
+     
